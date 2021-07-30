d4bcd261f88fd50316a8478286126ab8ffcc076b
samirkhanal35 <samirkhanal35@gmail.com>
2021-07-30 12:32:10 +0545

    Basic room messaging

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+let socket;
+const CONNECTION_PORT = "localhost:3001/";
 
 export default function App() {
-  return <div>react app</div>;
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [room, setRoom] = useState("");
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, [CONNECTION_PORT]);
+
+  const connectToRoom = () => {
+    socket.emit("join_room", room);
+  };
+
+  return (
+    <div className="App">
+      {!loggedIn ? (
+        <div className="logIn">
+          <div className="inputdivs">
+            <input
+              className="inputbox"
+              type="text"
+              placeholder="Name..."
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <input
+              className="inputbox"
+              type="text"
+              placeholder="Room..."
+              onChange={(e) => setRoom(e.target.value)}
+            />
+          </div>
+          <button onClick={connectToRoom} className="loginButton" type="submit">
+            Enter Chat
+          </button>
+        </div>
+      ) : (
+        <h1>you are not logged in</h1>
+      )}
+    </div>
+  );
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
-
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
+app.all("/", function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
-app.listen("3001", () => {
-  console.log("server running in port 3001");
+const server = app.listen("3001", () => {
+  console.log("Server Running on Port 3001...");
+});
+
+io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log("User Joined Room: " + data);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data.content);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("USER DISCONNECTED");
+  });
 });
